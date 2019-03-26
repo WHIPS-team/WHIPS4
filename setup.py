afb22c8fea68eae18141d9b2a7eb9928613133ec
Peidong Wang <pwang232@wisc.edu>
2019-03-26 17:15:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 REQUIRES = ['numpy', 'shapely', 'pyproj', 'tables', 'netCDF4', 'pyhdf']
 
 setup(name='WHIPS3',
-      version='3.0.0',
+      version='3.0.1',
       install_requires = REQUIRES,
       description='Scripts for customized regridding of Level-2 data to Level-3 data',
       license = 'MIT License',
@@ -12,7 +12,7 @@ setup(name='WHIPS3',
       packages=['process_sat'],
       scripts=['process_sat/whips.py'],
       url = 'https://nelson.wisc.edu/sage/data-and-models/software.php',
-      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.0.0.tar.gz',
+      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.0.1.tar.gz',
       classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
