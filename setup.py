aaa29b747666716c341c7fada98ef1ec4cef2fb0
Peidong Wang <pwang232@wisc.edu>
2019-03-27 14:31:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 REQUIRES = ['numpy', 'shapely', 'pyproj', 'tables', 'netCDF4', 'pyhdf']
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fp:
+with open(os.path.join(os.path.dirname(__file__), 'reST.rst')) as fp:
     long_description = fp.read()
 
 setup(name='WHIPS3',
-      version='3.0.6',
+      version='3.0.7',
       install_requires = REQUIRES,
       description='Scripts for customized regridding of Level-2 data to Level-3 data',
       long_description=long_description,
@@ -19,7 +19,7 @@ setup(name='WHIPS3',
       packages=['process_sat'],
       scripts=['process_sat/whips.py'],
       url = 'https://nelson.wisc.edu/sage/data-and-models/software.php',
-      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.0.6.tar.gz',
+      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.0.7.tar.gz',
       classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
