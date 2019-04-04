d5288a1e0ddfab90612e6292f2a460addf3d9026
Peidong Wang <pwang232@wisc.edu>
2019-04-04 17:31:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ def readme():
         return f.read()
 
 setup(name='WHIPS3',
-      version='3.1.0',
+      version='3.1.1',
       install_requires = REQUIRES,
       description='Scripts for customized regridding of Level-2 data to Level-3 data',
       long_description=readme(),
       license = 'MIT License',
-      author='Jacob Oberman, Keith Maki, Xiaomeng Jin, Peidong Wang, Elise Penn',
+      author='Jacob Oberman, Peidong Wang, Tracey Holloway',
       author_email='taholloway@wisc.edu',
       packages=['process_sat'],
       scripts=['process_sat/whips.py'],
       url = 'https://nelson.wisc.edu/sage/data-and-models/software.php',
-      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.1.0.tar.gz',
+      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.1.1.tar.gz',
       classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
