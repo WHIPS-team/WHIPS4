34c0af1149e141b23d02466e319ba10f0937d723
mkharkey <39805073+mkharkey@users.noreply.github.com>
2021-08-16 14:20:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
 
-REQUIRES = ['numpy', 'shapely', 'pyproj', 'tables', 'netCDF4', 'pyhdf']
+REQUIRES = ['cython','numpy', 'shapely', 'pyproj', 'pytables', 'netCDF4', 'jpeg','pyhdf']
 
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(name='WHIPS3',
-      version='3.1.1',
+setup(name='WHIPS4',
+      version='4.0',
       install_requires = REQUIRES,
       description='Scripts for customized regridding of Level-2 data to Level-3 data',
       long_description=readme(),
-      license = 'MIT License',
-      author='Tracey Holloway, Jacob Oberman, Peidong Wang',
+      author='Tracey Holloway, Jacob Oberman, Peidong Wang, Eliot Kim',
       author_email='taholloway@wisc.edu',
       packages=['process_sat'],
       scripts=['process_sat/whips.py'],
       url = 'https://nelson.wisc.edu/sage/data-and-models/software.php',
-      download_url='https://github.com/WHIPS-team/WHIPS3/archive/3.1.1.tar.gz',
+      download_url='https://github.com/WHIPS-team/WHIPS4/archive/refs/tags/v4.0.tar.gz',
       classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
